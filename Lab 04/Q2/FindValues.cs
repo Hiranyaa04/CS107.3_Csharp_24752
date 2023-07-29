1fcc64568ac3753bbc519acd4886855375e04340
Hiranyaa04 <136008357+Hiranyaa04@users.noreply.github.com>
2023-07-29 20:22:21 +0530

    Add files via upload

---
+++ Lab 04/Q2/FindValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4Q2
+{
+    internal class FindValues
+    {
+        public double findArea(double r)
+        { 
+            double Area = Math.PI * r * r;
+            return Area;
+        }
+
+        public double findCircumference(double r)
+        {
+            double circ = Math.PI * r * 2;
+            return circ;
+        }
+    }
+}
